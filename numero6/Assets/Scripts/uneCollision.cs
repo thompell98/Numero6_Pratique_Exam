502ac28febbe4c42a3ba71d7045c81b8a73a7384
DESKTOP-U046IK5\Thomas <pellthomas@hotmail.com>
2018-10-14 22:59:43 -0400

    Numero6 fait

---
+++ numero6/Assets/Scripts/uneCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uneCollision : MonoBehaviour {
+    public GameObject cubeBas;
+    public AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "cubeBas")
+        {
+            Destroy(cubeBas);
+            audio.Play();
+        }
+    }
+}
